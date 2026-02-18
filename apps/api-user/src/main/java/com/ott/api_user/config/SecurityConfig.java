594d94a69c75717451940d53b963bb4a4775fc41
marulog <gr214777@naver.com>
2026-02-18 14:21:22 +0900

    [FEAT]: admin, transcode 서버가 api-user depends 하도록 수정

---
+++ apps/api-user/src/main/java/com/ott/api_user/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.ott.api_user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/health/**", "/actuator/info").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+}
