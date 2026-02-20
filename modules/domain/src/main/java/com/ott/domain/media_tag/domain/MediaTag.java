430fdd4be6d7fe481ee271f8b36ae3e797fc8cf8
강승우 <phonil@naver.com>
2026-02-20 18:48:23 +0900

    [REFACTOR]: Media 관련 클래스 추가

---
+++ modules/domain/src/main/java/com/ott/domain/media_tag/domain/MediaTag.java
@@ -0,0 +1,39 @@
+package com.ott.domain.media_tag.domain;
+
+import com.ott.domain.common.BaseEntity;
+import com.ott.domain.media.domain.Media;
+import com.ott.domain.tag.domain.Tag;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Builder
+@Getter
+@Table(name = "media_tag")
+public class MediaTag extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_id", nullable = false)
+    private Tag tag;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "media_id", nullable = false)
+    private Media media;
+}
