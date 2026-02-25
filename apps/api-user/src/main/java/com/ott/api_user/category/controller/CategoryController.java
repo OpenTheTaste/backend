b6d14a248070841c9196e4c6c7472a47978b9421
marulog <gr214777@naver.com>
2026-02-25 19:28:17 +0900

    [FEAT]: 카테고리 아이디 1 이상 값 검증

--- apps/api-user/src/main/java/com/ott/api_user/category/controller/CategoryController.java
+++ apps/api-user/src/main/java/com/ott/api_user/category/controller/CategoryController.java
@@ -4,8 +4,10 @@ import com.ott.api_user.category.dto.response.CategoryResponse;
 import com.ott.api_user.category.service.CategoryService;
 import com.ott.api_user.tag.dto.response.TagResponse;
 import com.ott.common.web.response.SuccessResponse;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Validated
 @RequestMapping("/categories")
 @RequiredArgsConstructor
 public class CategoryController implements CategoryApi {
@@ -27,7 +30,7 @@ public class CategoryController implements CategoryApi {
 
     @GetMapping("{categoryId}/tags")
     public ResponseEntity<SuccessResponse<List<TagResponse>>> getTagsByCategory(
-            @PathVariable Long categoryId
+            @Positive @PathVariable Long categoryId
     ) {
         return ResponseEntity.ok(SuccessResponse.of(categoryService.getTagsByCategory(categoryId)));
     }
