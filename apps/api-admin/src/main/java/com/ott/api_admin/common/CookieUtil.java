08de8750b3fb5ffcde1791bdb4ea15ab3b2c65a3
marulog <gr214777@naver.com>
2026-02-28 23:40:18 +0900

    [FEAT]: 로컬용 CORS 설정 추가

--- apps/api-admin/src/main/java/com/ott/api_admin/common/CookieUtil.java
+++ apps/api-admin/src/main/java/com/ott/api_admin/common/CookieUtil.java
@@ -10,24 +10,24 @@ public class CookieUtil {
 
     public void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
         ResponseCookie cookie = ResponseCookie.from(name, value)
-                .domain("openthetaste.cloud")  // 로컬 테스트 시 주석처리!!!
+//                .domain("openthetaste.cloud")  // 로컬 테스트 시 주석처리!!!
                 .httpOnly(true) // JS 접근 차단 -> 크로스 사이트 스크립트 공격 대비
-                .secure(true) // HTTPS 요청만 허용
+                .secure(false) // HTTPS 요청만 허용
                 .path("/")  // 모든 경로로 전송
                 .maxAge(maxAge)
-                .sameSite("None")  // 크로스 사이트에 대해서 쿠키 전송 허용
+                .sameSite("Lax")  // 크로스 사이트에 대해서 쿠키 전송 허용
                 .build();
         response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
     }
 
     public void deleteCookie(HttpServletResponse response, String name) {
         ResponseCookie cookie = ResponseCookie.from(name, "")
-                .domain("openthetaste.cloud")  // 로컬 테스트 시 주석처리!!!
+//                .domain("openthetaste.cloud")  // 로컬 테스트 시 주석처리!!!
                 .httpOnly(true)
-                .secure(true)
+                .secure(false)
                 .path("/")
                 .maxAge(0)
-                .sameSite("None")
+                .sameSite("Lax")
                 .build();
         response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
     }
