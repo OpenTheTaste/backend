9316400d711ac7ce390777e8a93068fcf21bb716
강승우 <phonil@naver.com>
2026-03-09 09:30:36 +0900

    [FEAT]: Media Metrics 계산 스케줄러 설정

--- apps/api-user/src/main/java/com/ott/api_user/ApiUserApplication.java
+++ apps/api-user/src/main/java/com/ott/api_user/ApiUserApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @ComponentScan(basePackages = "com.ott")
 @EntityScan(basePackages = "com.ott.domain")
 @EnableJpaRepositories(basePackages = "com.ott.domain")
---
+++ apps/api-user/src/main/java/com/ott/api_user/media_matrics/batch/MetricsWriter.java
@@ -0,0 +1,21 @@
+package com.ott.api_user.media_matrics.batch;
+
+import com.ott.domain.media_metrics.repository.MediaMetricsJdbcRepository;
+import com.ott.domain.media_metrics.repository.MediaMetricsRow;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class MetricsWriter {
+
+    private final MediaMetricsJdbcRepository mediaMetricsJdbcRepository;
+
+    @Transactional
+    public void save(List<MediaMetricsRow> rowList) {
+        mediaMetricsJdbcRepository.bulkUpsert(rowList);
+    }
+}
