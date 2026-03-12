f8eb6e4be33e8aeb30b2118b1d4466707ec61df1
marulog <gr214777@naver.com>
2026-03-12 17:23:48 +0900

    [FEAT]: AI서버 호출 및 AI 태그 저장 로직

--- modules/domain/src/main/java/com/ott/domain/media_mood_tag/repository/MediaMoodTagRepository.java
+++ modules/domain/src/main/java/com/ott/domain/media_mood_tag/repository/MediaMoodTagRepository.java
@@ -3,8 +3,6 @@ package com.ott.domain.media_mood_tag.repository;
 import com.ott.domain.media_mood_tag.domain.MediaMoodTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface MediaMoodTagRepository extends JpaRepository<MediaMoodTag, Long> {
-
+    void deleteByMedia_Id(Long mediaId);
 }
--- modules/domain/src/main/java/com/ott/domain/mood_tag/repository/MoodTagRepository.java
+++ modules/domain/src/main/java/com/ott/domain/mood_tag/repository/MoodTagRepository.java
@@ -1,14 +1,12 @@
 package com.ott.domain.mood_tag.repository;
 
 import com.ott.domain.common.Status;
-import com.ott.domain.mood_category.domain.MoodCategory;
 import com.ott.domain.mood_tag.domain.MoodTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
 public interface MoodTagRepository extends JpaRepository<MoodTag, Long> {
 
+    List<MoodTag> findByNameInAndStatus(List<String> aiTags, Status status);
 }
