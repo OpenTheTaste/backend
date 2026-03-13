44cef71a5128d177f5c354276ea7e2cd09284039
arlen02-01 <lcglcg0201@gmail.com>
2026-03-13 20:01:01 +0900

    [OT-309] [FIX]: S3Client 빈 중복 정의 제거

--- modules/infra-s3/src/main/java/com/ott/infra/s3/config/S3PresignerConfig.java
+++ modules/infra-s3/src/main/java/com/ott/infra/s3/config/S3PresignerConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
@@ -14,14 +13,6 @@ public class S3PresignerConfig {
     @Bean
     public S3Presigner s3Presigner(@Value("${aws.region:ap-northeast-2}") String region) {
         return S3Presigner.builder()
-                .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.builder().build())
-                .build();
-    }
-
-    @Bean
-    public S3Client s3Client(@Value("${aws.region:ap-northeast-2}") String region) {
-        return S3Client.builder()
                 .region(Region.of(region))
                 .credentialsProvider(DefaultCredentialsProvider.builder().build())
                 .build();
