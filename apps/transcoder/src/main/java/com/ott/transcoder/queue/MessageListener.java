218ac0dce1274d9505af8374a57c1bdb2f98d277
강승우 <phonil@naver.com>
2026-04-22 10:14:31 +0900

    [FEAT]: 메시지 헤더 추가로 인한 리스너 시그니처 변경

--- apps/transcoder/src/main/java/com/ott/transcoder/queue/MessageListener.java
+++ apps/transcoder/src/main/java/com/ott/transcoder/queue/MessageListener.java
@@ -7,8 +7,10 @@ import com.ott.infra.mq.TranscodeMessage;
  *
  * 현재 구현체: RabbitTranscodeListener (RabbitMQ)
  * 큐 교체 시(SQS 등) 새 구현체만 추가하면 된다.
+ *
+ * @param delayed 이 메시지가 Delay Queue를 거쳐 재확인용으로 수신된 것인지 여부
  */
 public interface MessageListener {
 
-    void listen(TranscodeMessage message);
+    void listen(TranscodeMessage message, boolean delayed);
 }
